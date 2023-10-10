b7520b9581454fe520b308a5e7c4f477f5bfaad5
Diwas Rai <diwas.rai@kcl.ac.uk>
2023-10-10 04:22:59 +0100

    Coded frequencies method

--- frequencies.py
+++ frequencies.py
@@ -2,6 +2,23 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
+    if items == []:
+        return
+    
     frequencies = {}
     # Your code goes here
+    items = list(map(lambda item: str(item), items))
+    items.sort()
+    count = 1
+    lastUnique = items[0]
+
+    for i in range(1,len(items)):
+        if items[i] == lastUnique:
+            count += 1
+        else:
+            frequencies[str(items[i-1])] = count
+            lastUnique = items[i]
+            count = 1
+    frequencies[str(items[i])] = count
+
     return frequencies
